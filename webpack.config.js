1f3170e4e03b42f5b0a6e664a99b4e0eb35547e7
fengliyang <1690172939@qq.com>
2019-01-29 18:15:28 +0800

    提交更新文件

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ if(isDev){
    config.devtool = '#cheap-module-eval-source-map'
    config.devServer = {
       port:8000, //端口号
-      host:'0.0.0.0',
+      host:'0.0.0.0',//可用localhost:8000访问
       overlay:{
          errors:true
       },
